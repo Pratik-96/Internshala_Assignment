368f3dd4eb80ea489f1725b3be0fa71c2022fc86
AnonymousCoder1 <pratiknandvikar1404@gmail.com>
2024-11-21 14:45:02 +0530

    Completed Bottom Navigation Bar.

--- app/src/main/java/com/example/assignment/DataClasses/NavigationItem.kt
+++ app/src/main/java/com/example/assignment/DataClasses/NavigationItem.kt
@@ -6,4 +6,5 @@ import androidx.compose.ui.graphics.vector.ImageVector
 data class NavigationItem(
     val title:String,
     val selectedIcon : ImageVector,
+    val route : String
 )
--- app/src/main/java/com/example/assignment/MainActivity.kt
+++ app/src/main/java/com/example/assignment/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -18,17 +17,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.assignment.DataClasses.NavigationItem
+import com.example.assignment.Screens.ActivityScreen
+import com.example.assignment.Screens.CameraScreen
+import com.example.assignment.Screens.FeedScreen
+import com.example.assignment.Screens.GoalScreen
+import com.example.assignment.Screens.ProfileScreen
 import com.example.assignment.Screens.Screens
 import com.example.assignment.ui.theme.AssignmentTheme
 
@@ -44,29 +44,30 @@ class MainActivity : ComponentActivity() {
                     NavigationItem(
                         "Activity",
                         ImageVector.vectorResource(R.drawable.activity),
-                    ),NavigationItem(
+                        route = Screens.ActivityScreen.route
+                    ), NavigationItem(
                         "Goals",
                         ImageVector.vectorResource(R.drawable.graph),
-                    ),NavigationItem(
+                        route = Screens.GoalsScreen.route
+                    ), NavigationItem(
                         "Camera",
                         ImageVector.vectorResource(R.drawable.camera),
-                    ),NavigationItem(
+                        route = Screens.CameraScreen.route
+
+                    ), NavigationItem(
                         "Feed",
                         ImageVector.vectorResource(R.drawable.feed),
-                    ),NavigationItem(
+                        route = Screens.FeedScreen.route
+
+                    ), NavigationItem(
                         "Profile",
                         ImageVector.vectorResource(R.drawable.profile),
+                        route = Screens.ProfileScreen.route
+
                     )
                 )
 
                 val navController = rememberNavController()
-                NavHost(navController = navController, startDestination = Screens.ActivityScreen.route){
-                    composable(route = Screens.ActivityScreen.route) {
-
-                    }
-                }
-
-
 
 
                 var selectedItemIndex by rememberSaveable { mutableStateOf(0) }
@@ -80,19 +81,45 @@ class MainActivity : ComponentActivity() {
                                 selected = selectedItemIndex == index,
                                 onClick = {
                                     selectedItemIndex = index
+                                    navController.navigate(item.route)
                                 },
                                 icon = {
-                                    Icon(item.selectedIcon,null)
+                                    Icon(item.selectedIcon, null)
                                 },
                                 label = {
                                     Text(item.title)
                                 }
-                                )
+                            )
                         }
 
                     }
 
-                }) {innerPadding->  }
+                }) { innerPadding ->
+                    Column(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)) {
+                        NavHost(
+                            navController = navController,
+                            startDestination = Screens.ActivityScreen.route
+                        ) {
+                            composable(route = Screens.ActivityScreen.route) {
+                                ActivityScreen()
+                            }
+                            composable(route = Screens.GoalsScreen.route) {
+                                GoalScreen()
+                            }
+                            composable(route = Screens.CameraScreen.route) {
+                                CameraScreen()
+                            }
+                            composable(route = Screens.FeedScreen.route) {
+                                FeedScreen()
+                            }
+                            composable(route = Screens.ProfileScreen.route) {
+                                ProfileScreen()
+                            }
+                        }
+                    }
+                }
 
 
 //                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -115,10 +142,6 @@ class MainActivity : ComponentActivity() {
 //                      }
 
 
-
-
-
-
 //                  }
 //                }
             }
@@ -128,7 +151,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Homepage(modifier: Modifier = Modifier) {
-    
+
 }
 
 
